92ee2317f67cc3fdcb1b30c3b97891949950e198
rv3392 <richalverma00@gmail.com>
2020-09-11 23:00:12 +1000

    Add helper method to check if binary heap is complete
    - isComplete checks if the binary heap is complete
    - getMaxDepth gets the length of the path from the root to the left-most node which should be the longest path in a complete binary tree
    - completenessTest works with the current implementation of isStrongHeap and should be removed when isStrongHeap is actually implemented, this was just the best way to test isCompleteness without making it public

--- src/StrongHeap.java
+++ src/StrongHeap.java
@@ -1,4 +1,6 @@
 public class StrongHeap {
+    private static final int DEFAULT_DEPTH = -1;
+
     /**
      * Determines whether the binary tree with the given root node is
      * a "strong binary heap", as described in the assignment task sheet.
@@ -11,6 +13,76 @@ public class StrongHeap {
      * @return true if the tree is a strong heap, otherwise false.
      */
     public static boolean isStrongHeap(BinaryTree<Integer> root) {
-        return false; // TODO: implement question 2
+        int[] previousDepth = {DEFAULT_DEPTH}; // ugly hack for a mutable int
+        if (!isComplete(root, getMaxDepth(root), 0, previousDepth)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Gets what should be the longest root to leaf path if the BinaryTree defined by the given root is complete. For
+     * a complete binary tree this should be the path from the root to the left-most node.
+     *
+     * @param root Starting node (root) of the BinaryTree whose max depth is being found
+     * @return Longest root to leaf path of the BinaryTree
+     */
+    private static int getMaxDepth(BinaryTree<Integer> root) {
+        BinaryTree<Integer> currentNode = root;
+        int maxDepth = 0;
+        while (currentNode.getLeft() != null) {
+            maxDepth++;
+            currentNode = currentNode.getLeft();
+        }
+
+        return maxDepth;
+    }
+
+    /**
+     * Checks whether a BinaryTree is complete or not.
+     *
+     * Given a starting node, it recursively calls itself until a leaf node is reached. Upon reaching the leaf node,
+     * the method checks if the BinaryTree violates any of the requirements of a complete binary tree - that all
+     * nodes are on the left and that all levels apart from the last is completely filled.
+     *
+     * @param node The node in the binary tree to start from
+     * @param maxDepth The maximum depth of the binary tree - i.e. longest root to leaf path
+     * @param currentDepth The depth of the current node
+     * @param previousDepth The 1st element is the depth of the previous node with the previous node being the last node
+     *                      in terms of pre-order traversal - this is a very ugly workaround for a mutable int xd
+     * @return Whether the BinaryTree defined by the the root is complete or not
+     */
+    private static boolean isComplete(BinaryTree<Integer> node, int maxDepth, int currentDepth, int[] previousDepth) {
+        if (node.isLeaf()) {
+            if (currentDepth > maxDepth || currentDepth < maxDepth - 1
+                    || (currentDepth > previousDepth[0] && previousDepth[0] != DEFAULT_DEPTH)) {
+                // Set last depth to this node's depth.
+                previousDepth[0] = currentDepth;
+                return false;
+            }
+
+            previousDepth[0] = currentDepth;
+            return true;
+        } else {
+            if (node.getLeft() != null && node.getRight() != null) {
+                // If the current node isn't a leaf node then recursively call function to check if any child nodes
+                // violate the requirements for completeness. If either sub-tree has such a violation then pass false
+                // up the call chain.
+                return isComplete(node.getLeft(), maxDepth, currentDepth + 1, previousDepth)
+                        && isComplete(node.getRight(), maxDepth, currentDepth + 1, previousDepth);
+            } else if (node.getLeft() == null) {
+                // Left child should never be null if the right child isn't null
+                return false;
+            } else if (node.getRight() == null) {
+                // Make sure any future nodes don't have a depth deeper than this node as a complete binary tree
+                // should only have a single change of depths at the lowest level.
+                previousDepth[0] = currentDepth;
+                return true;
+            } else {
+                return true; // Should never actually happen - just here because the compiler doesn't like it otherwise.
+            }
+
+        }
     }
 }

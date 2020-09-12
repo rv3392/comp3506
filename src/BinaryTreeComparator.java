84479e443de994bdccaf7456263c8a36fa671b30
rv3392 <richalverma00@gmail.com>
2020-09-12 10:54:21 +1000

    Add BinaryTreeComparator using definition provided in assignment spec

--- src/BinaryTreeComparator.java
+++ src/BinaryTreeComparator.java
@@ -17,7 +17,30 @@ public class BinaryTreeComparator<E extends Comparable<E>> implements Comparator
      */
     @Override
     public int compare(BinaryTree<E> tree1, BinaryTree<E> tree2) {
-        // TODO: implement question 3 here
-        return 0;
+        // If either of the nodes given is null then the leaf of the tree has been reached
+        if (tree1 == null && tree2 == null) {
+            return 0;
+        } else if (tree1 == null) {
+            return -1;
+        } else if (tree2 == null) {
+            return 1;
+        }
+
+        // If all three comparisons return 0 (i.e. the two values are equal) then the subtree beginning from tree1
+        // and tree2 are both equal.
+
+        // Compare left subtree
+        int leftCompare = compare(tree1.getLeft(), tree2.getLeft());
+        if (leftCompare != 0) {
+            return leftCompare;
+        }
+        // Compare values
+        int valCompare = (tree1.getValue()).compareTo(tree2.getValue());
+        if (valCompare != 0) {
+            return valCompare;
+        }
+        // Compare right subtree
+        int rightCompare = compare(tree1.getRight(), tree2.getRight());
+        return rightCompare;
     }
 }

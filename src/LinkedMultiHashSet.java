7f3761cbcc2400108447c8e4275bf9b789fd608b
rv3392 <richalverma00@gmail.com>
2020-09-19 10:46:05 +1000

    Add basic LinkedMultiHashSet functionality without resizing
    - Can add and remove elements to/from the hashset however
      resizing has not been implemented yet.
    - Stores the current total number of elements including duplicates
      and number of distinct elements.
    - Duplicates are not stored in the hashtable array and so the array
      itself may contain nulls even if the numElements == capacity.
    - CountableElement is used to represent an element which can have
      duplicates.
    - Checks if the element is in the hashset already, if it is then the
      CountableElement used to represent that element is incremented.

--- src/LinkedMultiHashSet.java
+++ src/LinkedMultiHashSet.java
@@ -22,59 +22,190 @@ import java.util.NoSuchElementException;
  */
 public class LinkedMultiHashSet<T> implements MultiSet<T>, Iterable<T> {
 
-    // TODO: implement question 4 in this file
-    
+    private CountableElement<T>[] hashTable;
+
+    private int numElements;
+    private int numDistinctElements;
+    private int capacity;
+
+    @SuppressWarnings({"unchecked"})
     public LinkedMultiHashSet(int initialCapacity) {
-        
+        this.capacity = initialCapacity;
+        this.numElements = 0;
+        this.numDistinctElements = 0;
+
+        this.hashTable = (CountableElement<T>[]) new CountableElement[initialCapacity];
     }
 
     @Override
     public void add(T element) {
-        
+        if (this.numElements >= this.capacity) {
+            this.resize();
+        }
+
+        if (!this.contains(element)) {
+            int insertIndex = this.linearProbing(this.hash(element.hashCode()));
+            this.hashTable[insertIndex] = new CountableElement<T>(element);
+            this.numDistinctElements++;
+        } else {
+            int index = this.getElementIndexInTable(element);
+            this.hashTable[index].addToCount(1);
+        }
+
+        this.numElements++;
     }
 
     @Override
     public void add(T element, int count) {
-        
+        if (this.numElements >= this.capacity) {
+            this.resize();
+        }
+
+        if (!this.contains(element)) {
+            int insertIndex = this.linearProbing(this.hash(element.hashCode()));
+            this.hashTable[insertIndex] = new CountableElement<T>(element);
+            this.hashTable[insertIndex].addToCount(count - 1);
+            this.numDistinctElements++;
+        } else {
+            int index = this.getElementIndexInTable(element);
+            this.hashTable[index].addToCount(count);
+        }
+
+        this.numElements += count;
     }
 
     @Override
     public boolean contains(T element) {
-        return false;
+        if (this.getElementIndexInTable(element) == -1) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public int count(T element) {
-        return 0;
+        int elementIndex = this.getElementIndexInTable(element);
+        if (elementIndex == -1) {
+            return 0;
+        }
+
+        return this.hashTable[elementIndex].getCount();
     }
 
     @Override
     public void remove(T element) throws NoSuchElementException {
+        int elementIndex = this.getElementIndexInTable(element);
+        if (elementIndex == -1) {
+            throw new NoSuchElementException();
+        }
 
+        this.hashTable[elementIndex].addToCount(-1);
+        if (this.hashTable[elementIndex].getCount() == 0) {
+            this.hashTable[elementIndex] = null;
+            this.numDistinctElements--;
+        }
+
+        this.numElements--;
     }
 
     @Override
     public void remove(T element, int count) throws NoSuchElementException {
+        int elementIndex = this.getElementIndexInTable(element);
+        if (elementIndex == -1) {
+            throw new NoSuchElementException();
+        }
 
+        if (count > this.hashTable[elementIndex].getCount()) {
+            throw new NoSuchElementException();
+        }
+
+        this.hashTable[elementIndex].addToCount(-count);
+        if (this.hashTable[elementIndex].getCount() == 0) {
+            this.hashTable[elementIndex] = null;
+            this.numDistinctElements--;
+        }
+
+        this.numElements -= count;
     }
 
     @Override
     public int size() {
-        return -1;
+        return this.numElements;
     }
 
     @Override
     public int internalCapacity() {
-        return 0;
+        return this.capacity;
     }
 
     @Override
     public int distinctCount() {
-        return 0;
+        return this.numDistinctElements;
     }
 
     @Override
     public Iterator<T> iterator() {
         return null;
+    }
+
+    private void resize() {
+        //TODO: Implement this
+    }
+
+    private int hash(int key) {
+        return key % this.capacity;
+    }
+
+    private int linearProbing(int start) {
+        int i = start;
+        while (this.hashTable[i] != null) {
+            i++;
+        }
+
+        return i;
+    }
+
+    private int getElementIndexInTable(T element) {
+        int i = this.hash(element.hashCode());
+        if (this.hashTable[i] == null) {
+            return -1;
+        }
+
+        while (!this.hashTable[i].getValue().equals(element)) {
+            i++;
+            if (this.hashTable[i] == null) {
+                return -1;
+            }
+            if (i > this.numElements - 1) {
+                return -1;
+            }
+        }
+
+        return i;
+    }
+
+    private class CountableElement<T> {
+        T value;
+        int count;
+
+        CountableElement (T value) {
+            this.value = value;
+            this.count = 1;
+        }
+
+        void addToCount(int i) {
+            this.count += i;
+        }
+
+        int getCount() {
+            return this.count;
+        }
+
+        T getValue() {
+            return this.value;
+        }
+
+
     }
 }

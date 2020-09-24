ce1b1b1b66bc0571ed9dca00c6326ce8a3199a0e
rv3392 <richalverma00@gmail.com>
2020-09-24 18:44:08 +1000

    Fix issue with not returning -1,0,1 and add runtime

--- src/BinaryTreeComparator.java
+++ src/BinaryTreeComparator.java
@@ -11,6 +11,11 @@ public class BinaryTreeComparator<E extends Comparable<E>> implements Comparator
      * Two nodes are compared by their left childs, their values, then their right childs,
      * in that order. A null is less than a non-null, and equal to another null.
      *
+     * The worst case time complexity for this method is O(n) where n is the size of the
+     * larger of the two trees. This is because in the worst case every element in both
+     * trees must be compared. The worst case memory complexity is O(1) as no extra memory
+     * is required to run this method.
+     *
      * @param tree1 root of the first binary tree, may be null.
      * @param tree2 root of the second binary tree, may be null.
      * @return -1, 0, +1 if tree1 is less than, equal to, or greater than tree2, respectively.
@@ -37,9 +42,9 @@ public class BinaryTreeComparator<E extends Comparable<E>> implements Comparator
         // Compare values
         int valCompare = (tree1.getValue()).compareTo(tree2.getValue());
         if (valCompare != 0) {
-            return valCompare;
+            return valCompare > 0 ? 1 : -1;
         }
-        // Compare right subtree
+        // Compare right subtree and return the result since left and value were both equal
         int rightCompare = compare(tree1.getRight(), tree2.getRight());
         return rightCompare;
     }

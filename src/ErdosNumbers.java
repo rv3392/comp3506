b71f3e1ce125f2223c7f0e16f13d5c30a87ae4b9
rv3392 <richalverma00@gmail.com>
2020-10-23 11:08:45 +1000

    Fix style and documentation for ErdosNumbers.java

--- src/ErdosNumbers.java
+++ src/ErdosNumbers.java
@@ -32,92 +32,6 @@ public class ErdosNumbers {
             this.parsePaper(paper);
         }
     }
-
-    private void parsePaper(String paper) {
-        String[] details = paper.split(":");
-        String paperName = details[0];
-        String[] authorNames = details[1].split("\\|");
-
-        papers.put(paperName, Arrays.asList(authorNames));
-
-        for (int i = 0; i < authorNames.length; i++) {
-            String authorName = authorNames[i];
-            this.parseAuthor(paperName, authorName);
-            for (int j = i + 1; j < authorNames.length; j++) {
-                String otherAuthorName = authorNames[j];
-                this.parseAuthor(paperName, otherAuthorName);
-                this.parseCollaboration(paperName, authorName, otherAuthorName);
-            }
-
-        }
-    }
-
-    private void parseAuthor(String paperName, String authorName) {
-        this.authors.putIfAbsent(authorName, new Author(authorName));
-        this.authors.get(authorName).addPaper(paperName);
-    }
-
-    private void parseCollaboration(String paperName, String authorName, String otherAuthorName) {
-        Author first = this.authors.get(authorName);
-        Author second = this.authors.get(otherAuthorName);
-        first.addCollaboration(second.getName());
-        second.addCollaboration(first.getName());
-    }
-
-    private class Author {
-        private String name;
-        private Map<String, Integer> collaborations;
-        private Set<String> papers;
-
-        private Author(String name) {
-            this.name = name;
-            this.collaborations = new HashMap<String, Integer>();
-            this.papers = new HashSet<String>();
-        }
-
-        private String getName() {
-            return this.name;
-        }
-
-        private void addCollaboration(String collaborator) {
-            this.collaborations.put(collaborator, this.collaborations.getOrDefault(collaborator, 0) + 1);
-        }
-
-        private int getCollaborationCount(String collaborator) {
-            return collaborations.get(collaborator);
-        }
-
-        private void addPaper(String paper) {
-            this.papers.add(paper);
-        }
-
-        private Set<String> getPapers() {
-            return this.papers;
-        }
-
-        private Set<String> getCollaborators() {
-            return this.collaborations.keySet();
-        }
-    }
-
-    private class ComparableWeightedAuthor implements Comparable {
-        private Author author;
-        private double priority;
-
-        private ComparableWeightedAuthor(Author author, double priority) {
-            this.author = author;
-            this.priority = priority;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            if (!(o instanceof ComparableWeightedAuthor)) {
-                throw new ClassCastException();
-            }
-
-            return Double.compare(this.priority, ((ComparableWeightedAuthor) o).priority);
-        }
-    }
     
     /**
      * Gets all the unique papers the author has written (either solely or
@@ -233,6 +147,174 @@ public class ErdosNumbers {
         return djikstraShortestPathLength(new PriorityQueue<>(), ERDOS, author, false);
     }
 
+    /**
+     * Parses a paper and adds any new authors found to this.authors and the paper to this.papers. Each involved
+     * author's collaboration Map is also updated to reflect the new collaborators.
+     * @param paper the full string representing a particular paper including the authors
+     */
+    private void parsePaper(String paper) {
+        String[] details = paper.split(":");
+        String paperName = details[0];
+        String[] authorNames = details[1].split("\\|");
+
+        papers.put(paperName, Arrays.asList(authorNames));
+
+        for (int i = 0; i < authorNames.length; i++) {
+            String authorName = authorNames[i];
+            this.parseAuthor(paperName, authorName);
+            for (int j = i + 1; j < authorNames.length; j++) {
+                String otherAuthorName = authorNames[j];
+                this.parseAuthor(paperName, otherAuthorName);
+                this.parseCollaboration(authorName, otherAuthorName);
+            }
+
+        }
+    }
+
+    /**
+     * Parses a particular author for a particular paper and adds it to this.authors if they aren't already in it. In
+     * both cases the name of the paper is added to the new or existing author.
+     * @param paperName the paper the author worked on
+     * @param authorName the name of the author
+     */
+    private void parseAuthor(String paperName, String authorName) {
+        this.authors.putIfAbsent(authorName, new Author(authorName));
+        this.authors.get(authorName).addPaper(paperName);
+    }
+
+    /**
+     * Parses a collaboration between two authors and adds it to both authors.
+     * @param authorName the name of the first author
+     * @param otherAuthorName the name of the second author
+     */
+    private void parseCollaboration(String authorName, String otherAuthorName) {
+        Author first = this.authors.get(authorName);
+        Author second = this.authors.get(otherAuthorName);
+        first.addCollaboration(second.getName());
+        second.addCollaboration(first.getName());
+    }
+
+    /**
+     * A class representing a particular Author. Since each Author has a unique name, this class uses name as a unique
+     * identifier. It also stores all of the papers an author has worked on as well as the name of author's they
+     * have collaborated with and the amount of collaborations.
+     */
+    private class Author {
+        /* Instance of an Author that needs to be compared */
+        private String name;
+        /* A Map between collaborators' names and the number of times they have collaborated with this author */
+        private Map<String, Integer> collaborations;
+        /* A Set of all of the papers this author has worked on */
+        private Set<String> papers;
+
+        /**
+         * Constructor for Author.
+         * @param name the unique name of an author
+         */
+        private Author(String name) {
+            this.name = name;
+            this.collaborations = new HashMap<String, Integer>();
+            this.papers = new HashSet<String>();
+        }
+
+        /**
+         * Get the name of the author.
+         * @return the name of the author
+         */
+        private String getName() {
+            return this.name;
+        }
+
+        /**
+         * Adds a collaboration with the given author name. The author name should be the same as is used elsewhere.
+         * If a collaboration with this author has been added before, a counter of the two authors' collaborations
+         * is kept.
+         * @param collaborator the unique name of the collaborating author
+         */
+        private void addCollaboration(String collaborator) {
+            this.collaborations.put(collaborator, this.collaborations.getOrDefault(collaborator, 0) + 1);
+        }
+
+        /**
+         * Gets the number of this author has collaborated with the author with the given name.
+         * @param collaborator
+         * @return the number of times this author has collaborated with the author with the given name if they have
+         *      collaborated before, otherwise a null
+         */
+        private int getCollaborationCount(String collaborator) {
+            return collaborations.get(collaborator);
+        }
+
+        /**
+         * Adds a new paper to this author's set of papers they have worked on.
+         * @param paper the paper to add to the set
+         */
+        private void addPaper(String paper) {
+            this.papers.add(paper);
+        }
+
+        /**
+         * Gets a set of papers that this author has worked on in this past.
+         * @return a set of strings representing the papers that this author has worked on
+         */
+        private Set<String> getPapers() {
+            return this.papers;
+        }
+
+        /**
+         * Gets a set of collaborators (authors) that this author has worked with. This set contains the names of the
+         * authors not an Author representation.
+         * @return a set of strings representing the names of all the authors this author has collaborated with
+         */
+        private Set<String> getCollaborators() {
+            return this.collaborations.keySet();
+        }
+    }
+
+    /**
+     * A comparable wrapper class for an Author instance. It should store the distance from Erdos within the graph
+     * representation and use this as the basis of comparison.
+     */
+    private class ComparableWeightedAuthor implements Comparable {
+        /* Instance of an Author that needs to be compared */
+        private Author author;
+        /* The weight given to this Author (distance from Erdos in this case) */
+        private double priority;
+
+        /**
+         * Constructor for ComparableWeightedAuthor.
+         * @param author an Author instance that should be wrapped
+         * @param priority the priority given to the Author - used for comparing ComparableWeightedAuthors
+         */
+        private ComparableWeightedAuthor(Author author, double priority) {
+            this.author = author;
+            this.priority = priority;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if (!(o instanceof ComparableWeightedAuthor)) {
+                throw new ClassCastException();
+            }
+
+            return Double.compare(this.priority, ((ComparableWeightedAuthor) o).priority);
+        }
+    }
+
+    /**
+     * Runs Djikstra's algorithm on the Authors graph stored in this.author and gives the shortest path length
+     *
+     * Begins at start and tries to reach end using the given Queue "frontier" as the discovery edge or frontier.
+     * If "unweighted" is set to true then this essentially acts as a normal BFS and a LinkedList can be passed to
+     * "frontier". To use in Djikstra mode, set "unweighted" to false and pass a PriorityQueue to "frontier".
+     *
+     * @param frontier the discovery edge data structure to use
+     * @param start the name of the author to start from
+     * @param end the name of the author to try and reach
+     * @param unweighted whether weights should be used for the edges - i.e. if true then a BFS is run otherwise
+     *                   Djikstra's algorithm is run
+     * @return the length of the shortest path between start and end in the stored graph
+     */
     private double djikstraShortestPathLength(Queue<ComparableWeightedAuthor> frontier, String start, String end,
              boolean unweighted) {
         Set<String> visited = new HashSet<>();
@@ -272,6 +354,14 @@ public class ErdosNumbers {
         return Double.MAX_VALUE;
     }
 
+    /**
+     * Gets the weight of a particular edge using the formula given in the tasksheet. If "unweighted" is passed as true
+     * then this method always returns 1.0d.
+     * @param start the name of the author to start from
+     * @param end the name of the author to end at
+     * @param unweighted whether a weighted or unweighted weight is required
+     * @return the weight of a particular edge in the stored graph
+     */
     private double getEdgeWeight(String start, String end, boolean unweighted) {
         if (unweighted) {
             return 1.0d;

831ff8b9687d61adaca52b43ba6c1fc70da11062
rv3392 <richalverma00@gmail.com>
2020-10-28 14:47:11 +1000

    Fix style and documentation in ContactTracer.java

--- src/ContactTracer.java
+++ src/ContactTracer.java
@@ -1,77 +1,13 @@
 import java.util.*;
 
 public class ContactTracer {
-
+    /**
+     *  A map of a person's name to their unique instance representation.
+     *  Stores a graph representation of all people and their contacts.
+     */
     private Map<String, Person> people;
-
+    /* The amount of time in minutes it takes for a person to become contagious */
     private final int TIME_TO_CONTAGIOUS = 60;
-
-    private class Person {
-        private String name;
-        private Map<String, Contact> contacts;
-
-        private Person(String name) {
-            this.name = name;
-            this.contacts = new HashMap<>();
-        }
-
-        private String getName() {
-            return this.name;
-        }
-
-        private void addNewContact(Person otherPerson, int time) {
-            if (this.contacts.containsKey(otherPerson.getName())) {
-                this.contacts.get(otherPerson.getName()).addNewTime(time);
-                return;
-            }
-
-            Contact toAdd = new Contact(this, otherPerson, time);
-            this.contacts.put(otherPerson.getName(), toAdd);
-            otherPerson.contacts.put(this.getName(), toAdd);
-        }
-
-        private Map<String, Contact> getAllContacts() {
-            return this.contacts;
-        }
-
-        @Override
-        public int hashCode() {
-            return this.name.hashCode();
-        }
-    }
-
-    private class Contact{
-        private Person person1;
-        private Person person2;
-
-        private int latestContactTime;
-        private Set<Integer> contactTimes;
-
-        private Contact(Person person1, Person person2, int time) {
-            this.person1 = person1;
-            this.person2 = person2;
-
-            this.latestContactTime = time;
-
-            this.contactTimes = new HashSet<>();
-            this.contactTimes.add(time);
-        }
-
-        private int getLatestTime() {
-            return this.latestContactTime;
-        }
-
-        private Set<Integer> getContactTimes() {
-            return this.contactTimes;
-        }
-
-        private void addNewTime(int time) {
-            this.contactTimes.add(time);
-            if (time > this.latestContactTime) {
-                this.latestContactTime = time;
-            }
-        }
-    }
 
     /**
      * Initialises an empty ContactTracer with no populated contact traces.
@@ -214,5 +150,124 @@ public class ContactTracer {
         }
 
         return infectedContacts;
+    }
+
+    /**
+     * An instance representing a unique person.
+     */
+    private class Person {
+        /* The unique name of this person. */
+        private String name;
+        /* All of the contacts of this person */
+        private Map<String, Contact> contacts;
+
+        /**
+         * Conctructor for Person.
+         * @param name the unique name of this person
+         */
+        private Person(String name) {
+            this.name = name;
+            this.contacts = new HashMap<>();
+        }
+
+        /**
+         * Gets the name of this person.
+         * @return the name of this person
+         */
+        private String getName() {
+            return this.name;
+        }
+
+        /**
+         * Adds a new contact to this person's contacts. If this person has already had contact with
+         * otherPerson, then the time is simply added to the existing Contact instance.
+         * @param otherPerson the unique name of the otherPerson
+         * @param time the time at which this contact occurs
+         */
+        private void addNewContact(Person otherPerson, int time) {
+            // If this contact has occurred before just add the time to the existing Contact.
+            if (this.contacts.containsKey(otherPerson.getName())) {
+                this.contacts.get(otherPerson.getName()).addNewTime(time);
+                return;
+            }
+
+            Contact toAdd = new Contact(this, otherPerson, time);
+            this.contacts.put(otherPerson.getName(), toAdd);
+            otherPerson.contacts.put(this.getName(), toAdd);
+        }
+
+        /**
+         * Gets a map of all contacts.
+         * @return all of the contacts of this person
+         */
+        private Map<String, Contact> getAllContacts() {
+            return this.contacts;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.name.hashCode();
+        }
+    }
+
+    /**
+     * An instance representing the Contact between two Person instances.
+     */
+    private class Contact{
+        /* The person instance representing the first person in the contact */
+        private Person person1;
+        /* The person instance representing the second person in the contact */
+        private Person person2;
+
+        /* The latest time of contact between the two people of this Contact instance */
+        private int latestContactTime;
+        /* All of the times of contacts between these two people */
+        private Set<Integer> contactTimes;
+
+        /**
+         * Constructor for Contact.
+         *
+         * Creates an instance of Contact between person1 and person2 at a given time.
+         *
+         * @param person1 the instance representing the first person
+         * @param person2 the instance representing the second person
+         * @param time the time at which to create this Contact in the grpah
+         */
+        private Contact(Person person1, Person person2, int time) {
+            this.person1 = person1;
+            this.person2 = person2;
+
+            this.latestContactTime = time;
+
+            this.contactTimes = new HashSet<>();
+            this.contactTimes.add(time);
+        }
+
+        /**
+         * Gets the latest time of contact between person1 and person2 of this Contact.
+         * @return the latest time
+         */
+        private int getLatestTime() {
+            return this.latestContactTime;
+        }
+
+        /**
+         * Gets a Set of all contact times between person1 and person2.
+         * @return a Set of all contact times between the two people of this Contact
+         */
+        private Set<Integer> getContactTimes() {
+            return this.contactTimes;
+        }
+
+        /**
+         * Adds another contact time to this Contact.
+         * @param time the new contact time to add in minutes from start
+         */
+        private void addNewTime(int time) {
+            this.contactTimes.add(time);
+            if (time > this.latestContactTime) {
+                this.latestContactTime = time;
+            }
+        }
     }
 }
